9867c186b976fd4c2a1f81cc7a21dcf6ae4d7d3a
yuraMaluga <mr.georgeman97@gmail.com>
2020-02-16 16:30:37 +0200

    initial project structure;

---
+++ DocumentManagement.Models/Document.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentManagement.Models
+{
+    public class Document
+    {
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public int FileSize { get; set; }
+    }
+}
---
+++ DocumentManagement.WebApi/Controllers/DocumentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "files1", "files2" };
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            var size = file.Length;
+            return Ok();
+        }
+    }
+}
